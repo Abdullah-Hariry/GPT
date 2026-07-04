92aef32cf9fb756da0b7cdf543ad0e0641537472
Abdullah <abdullahhariry@Outlook.com>
2026-07-05 00:41:38 +0300

    added comments so my dumbass understands

--- gpt.py
+++ gpt.py
@@ -28,7 +28,7 @@ def encode(s):
 def decode(s):
     return ''.join([i_to_s[i] for i in s])
 
-data = torch.tensor(encode(text), dtype=torch.long)
+data = torch.tensor(encode(text), dtype=torch.long) # puts data in
 
 # Validation
 # Splits data into 9:1 ratio 9 being the data the transformer will train on and 1 being the data it will use to validate its results
@@ -40,22 +40,28 @@ val_data = data[n:]
 block_size = 8
 print(train_data[:block_size+1])
 
-x = train_data[:block_size]
-y = train_data[1:block_size+1]
+x = train_data[:block_size] # [..., [8]]
+y = train_data[1:block_size+1] # [[1] ... [9]]
 
-for t in range(block_size):
-    context = x[:t+1]
-    target = y[t]
+# for t in range(block_size):
+  #  context = x[:t+1] # t=1 context = [..., [2]] in the set of [..., [8]]
+  #  target = y[t] # t=1, target = [[2]] in the set of [[1]... [9]]
 
 batch_size = 4 # How many independent sequences will we process in parallel
-#generate a small of batch of data of inputs x and targets y
 
 torch.manual_seed(1337)
+"""
+get_batch() samples a batch by
+1. Picking 4 random spots in the text
+2. Cutting 8 characters at each spot we call this x (context)
+3. Cutting 8 characters at the same spot but shifted by 1 this is y (target)
+4. Stacking these 4 chunks into a single tensor
+"""
 def get_batch(split):
-    data = train_data if split == 'train' else val_data
-    ix = torch.randint(len(data) - block_size, (batch_size,))
-    x = torch.stack([data[i:i+block_size] for i in ix])
-    y = torch.stack([data[i+1:i+block_size+1] for i in ix])
+    data = train_data if split == 'train' else val_data # train_data = data[:n], hence data = data[:n] n being 90%
+    ix = torch.randint(len(data) - block_size, (batch_size,)) # ix = (length(data[:n]) - 8
+    x = torch.stack([data[i:i+block_size] for i in ix]) # x = []
+    y = torch.stack([data[i+1:i+block_size+1] for i in ix]) # y = []
     return x, y
 xb, yb = get_batch('train')
 
@@ -84,6 +90,10 @@ class BigramLanguageModel(nn.Module):
         logits = self.token_embedding_table(idx) # (B,T,C)
 
         return logits
+
+m = BigramLanguageModel(vocab_size)
+out = m(xb, yb)
+print(out.shape)
 
 m = BigramLanguageModel(vocab_size)
 out = m(xb, yb)
